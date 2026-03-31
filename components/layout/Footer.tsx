6040d07b97b006672a0264859ee05920acc2e037
carmaa <crfrencho@gmail.com>
2026-03-31 11:26:49 -0400

    mobile responsived nav and footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,16 +4,16 @@ import { Instagram, Facebook } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="mt-auto bg-[var(--footer)] border-t border-gray-300">
-      <div className="mx-auto px-8 py-5">
+      <div className="mx-auto px-6 md:px-8  py-4 md:py-5">
         <div className="flex items-center justify-between">
           {/*left*/}
-          <div className="text-[var(--text)] font-medium">
+          <div className="text-[var(--text)] font-medium text-sm md:text-base">
             Beacon Street Gardens{" "}
             <span className="text-xs font-normal">LLC</span>
           </div>
 
           {/*right*/}
-          <div className="flex items-center gap-4 text-[var(--text)]">
+          <div className="flex items-center gap-2 md:gap-4 text-[var(--text)] text-sm md:text-base">
             <Link
               href="/contact"
               className="hover:text-[var(--rust)] transition-colors "
@@ -21,22 +21,22 @@ export default function Footer() {
               Contact
             </Link>
             <span className="text-gray-400">|</span>
-            <span>Follow Us</span>
+            <span className="hidden md:inline">Follow Us</span>
             <a
               href="https://www.instagram.com/crfrencho/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-[var(--rust)] transition-colors flex items-center gap-3"
+              className="hover:text-[var(--rust)] transition-colors flex items-center"
             >
-              <Instagram size={20} />
+              <Instagram size={16} className="md:w-5 md:h-5"/>
             </a>
             <a
               href="https://www.instagram.com/crfrencho/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-[var(--rust)] transition-colors flex items-center gap-3"
+              className="hover:text-[var(--rust)] transition-colors flex items-center"
             >
-              <Facebook size={20} />
+              <Facebook size={16} className="md:w-5 md:h-5"/>
             </a>
           </div>
         </div>
